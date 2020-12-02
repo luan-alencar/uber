3fd400e71538a4a3427767c2c5cb364d88a30626
luan-alencar <luanalencar134@gmail.com>
2020-12-02 18:33:59 -0300

    aplicando testes

--- src/test/java/david/augusto/luan/CorridaTest.java
+++ src/test/java/david/augusto/luan/CorridaTest.java
@@ -1,8 +1,13 @@
 package david.augusto.luan;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import david.augusto.luan.entidades.UberBlack;
+import david.augusto.luan.entidades.UberPool;
 import david.augusto.luan.entidades.UberSistema;
 import david.augusto.luan.entidades.UberX;
 
@@ -11,10 +16,19 @@ public class CorridaTest {
 	@Autowired
 	private UberSistema uberSistema;
 	
-
 	@BeforeEach
 	void setUp() {
 		uberSistema.adicionarCorrida(new UberX(5));
+		uberSistema.adicionarCorrida(new UberBlack(10));
+		uberSistema.adicionarCorrida(new UberPool(20, 3));
+		uberSistema.adicionarCorrida(new UberPool(20, 3));
+	}
+//	@Autowired
+//	private UberX uberX;
+
+	@Test
+	void adicionarCorridaTest() {
+		assertEquals(uberSistema.getCorridas(), 4);
 	}
 	
 }
