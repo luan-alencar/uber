d0267126058fd7c4bce5ed6012916501493e154a
luan-alencar <luanalencar134@gmail.com>
2020-12-02 22:58:29 -0300

    removento RunWith

--- src/test/java/david/augusto/luan/CorridaTest.java
+++ src/test/java/david/augusto/luan/CorridaTest.java
@@ -3,15 +3,12 @@ package david.augusto.luan;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import david.augusto.luan.entidades.UberBlack;
 import david.augusto.luan.entidades.UberPool;
 import david.augusto.luan.entidades.UberSistema;
 import david.augusto.luan.entidades.UberX;
 
-@RunWith(SpringRunner.class)
 public class CorridaTest {
 
 	UberSistema uberSistema = new UberSistema();;
